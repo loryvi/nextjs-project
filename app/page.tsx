9fc2c4f33cf14d4ccfc939653a97aecca401ebdd
loryvi <lilyleogo@gmail.com>
2023-12-01 01:21:56 +0800

    fetch data and added dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -29,6 +29,7 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+          {/*Add hero image here*/}
           <Image 
             src ="/hero-desktop.png" 
             width={1000}
